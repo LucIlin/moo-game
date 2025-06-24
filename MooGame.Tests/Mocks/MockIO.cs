e10ff6a61462e5081ff292be4bb2cc0f4110aaed
Luclin <lucas.lindgren@outlook.com>
2025-06-24 16:21:18 +0200

    refactor: move MockIO to separate file

---
+++ MooGame.Tests/Mocks/MockIO.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using MooGame.App;
+
+namespace MooGame.Tests;
+
+internal class MockIO : IInputOutput
+{
+    Queue<string> _mockInputs { get; set; }
+   
+    public MockIO(Queue<string> mockInputs)
+    {
+        _mockInputs = mockInputs;
+    }
+
+    public string ReadInput() => _mockInputs.Count > 0 ? _mockInputs.Dequeue() : string.Empty;
+
+    public void WriteOutput(string output) => Debug.WriteLine(output);
+}
--- MooGame.Tests/Test1.cs
+++ MooGame.Tests/Test1.cs
@@ -1,6 +1,5 @@
 ﻿using MooGame.App;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Runtime.ExceptionServices;
 
 namespace MooGame.Tests;
@@ -37,18 +36,4 @@ public sealed class AppTest
         //Assert.AreEqual();
 
     }
-}
-
-internal class MockIO : IInputOutput
-{
-    Queue<string> _mockInputs { get; set; }
-   
-    public MockIO(Queue<string> mockInputs)
-    {
-        _mockInputs = mockInputs;
-    }
-
-    public string ReadInput() => _mockInputs.Count > 0 ? _mockInputs.Dequeue() : string.Empty;
-
-    public void WriteOutput(string output) => Debug.WriteLine(output);
 }
