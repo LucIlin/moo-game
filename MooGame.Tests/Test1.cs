e709808cb7b89a70325829fc000d5efcf9df3232
Xanderelium <alexander_suleymanov@yahoo.se>
2025-06-24 15:52:19 +0200

    refactor: create mockui for test project

--- MooGame.Tests/Test1.cs
+++ MooGame.Tests/Test1.cs
@@ -1,18 +1,32 @@
 ﻿using MooGame.App;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
 
 namespace MooGame.Tests;
 
-[TestCategory("Not Implemented")]
+
+
 [TestClass]
-public sealed class AppControllerTest
+public sealed class AppTest
 {
     [TestMethod]
-    public void TestIfRunIsExecutable()
+    [TestCategory("Not Implemented")]
+    public void TestIfControllerGetsInputs()
     {
 
         //Arrange
 
-        var controller = new AppController();
+        Queue<string> queue = new Queue<string>();
+        queue.Enqueue("lucas");
+        queue.Enqueue("42");
+        queue.Enqueue("");
+
+
+
+        var mockIO = new MockIO(queue);
+        var controller = new AppController(mockIO);
+
 
         //Act
 
@@ -20,6 +34,21 @@ public sealed class AppControllerTest
 
         //Assert
 
-        Assert.IsFalse();
+        //Assert.AreEqual();
+
     }
+}
+
+internal class MockIO : IInputOutput
+{
+    Queue<string> _mockInputs { get; set; }
+   
+    public MockIO(Queue<string> mockInputs)
+    {
+        _mockInputs = mockInputs;
+    }
+
+    public string ReadInput() => _mockInputs.Count > 0 ? _mockInputs.Dequeue() : string.Empty;
+
+    public void WriteOutput(string output) => Debug.WriteLine(output);
 }
