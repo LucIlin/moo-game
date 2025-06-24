da53dca00eb24eef5f1b20eafed8a18b48fccfb3
Alexander Suleymanov <alexander_suleymano97@yahoo.se>
2025-06-24 13:30:52 +0200

    chore: Create test project and reference the main project to the test project

---
+++ MooGame.Tests/Test1.cs
@@ -0,0 +1,25 @@
+﻿using MooGame.App;
+
+namespace MooGame.Tests;
+
+[TestCategory("Not Implemented")]
+[TestClass]
+public sealed class AppControllerTest
+{
+    [TestMethod]
+    public void TestIfRunIsExecutable()
+    {
+
+        //Arrange
+
+        var controller = new AppController();
+
+        //Act
+
+        controller.Run();
+
+        //Assert
+
+        Assert.IsFalse();
+    }
+}
