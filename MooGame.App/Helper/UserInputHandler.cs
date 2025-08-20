faeb2e40bbf250c26ef97c1573afd02c639f6c9f
Luclin <lucas.lindgren@outlook.com>
2025-08-20 18:17:51 +0200

    refactor: implemented input validation logic in UserInputHandler.cs

--- MooGame.App/Helper/UserInputHandler.cs
+++ MooGame.App/Helper/UserInputHandler.cs
@@ -1,25 +1,84 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using MooGame.App.Interfaces;
 
 namespace MooGame.App.Helper;
 
-public static class UserInputHandler
+public class UserInputHandler : IUserInput
 {
-    public static bool IsValidChars(string? input)
-    {   
-        if (string.IsNullOrEmpty(input)) throw new ArgumentException("Input is Null or Empty");
-        
-        if (input.Length > 25) throw new ArgumentException("Input length larger than 25 characters.");
-        
-        if (!Regex.IsMatch(input, @"^[a-zA-Z0-9!""#$%&'()*+, \-./:;<=>?@[\\\]^_`{|}~]+$")) throw new ArgumentException("Invalid characters");
-        
-        return true;
-    }
-
-    public static bool IsYesNo(string input)
+    private IInputOutput _io;
+    public UserInputHandler(IInputOutput io)
     {
-        input = input.ToLower();
-
-        return input is "n" or "y";
+        _io = io;
+    }
+    private static (bool, string?) IsValidChars(string? input)
+    {   
+        if (string.IsNullOrEmpty(input)) return (false, "Input is Null or Empty");
+        
+        if (input.Length > 25) return (false, "Input length larger than 25 characters.");
+        
+        if (!Regex.IsMatch(input, @"^[a-zA-Z0-9!""#$%&'()*+, \-./:;<=>?@[\\\]^_`{|}~]+$")) 
+            return (false, "Invalid characters");
+        
+        return (true, null);
     }
 
+    /// <summary>
+    /// Returns true for "Yes" and false for "No", throws argument exception if input is not N,n or Y,y.
+    /// </summary>
+    public bool GetYesNo(string? question = null)
+    {
+        Func<string, bool> validator = x => x is "y" or "n";
+        
+        var input = GetInput(prompt: question, customValidator: validator, errorMessage:"Input is not \"n\" or \"y\"");
+
+        if (input is "y")
+            return true;
+        if (input is "n")
+            return false;
+        
+        throw new UnreachableException($"Unexpected input: {input}");
+    }
+
+    public string GetInput(
+        string? prompt = null,
+        string? errorMessage = null,
+        Func<string, bool>? customValidator = null
+        )
+    {
+        bool isResolved = false;
+        string input  = string.Empty;
+        
+        if (prompt != null)
+            _io.WriteOutput(prompt);
+        
+        while (!isResolved)
+        {
+            input = _io.ReadInput() ?? "";
+            
+            var (isValidInput, error) = IsValidChars(input);
+
+            if (!isValidInput)
+            {
+                var charValidationError = error ?? "Invalid characters";
+                _io.WriteOutput(charValidationError);
+                continue;
+            }
+            
+            if (customValidator != null)
+            {
+                var isCustomValidated = customValidator(input);
+
+                if (!isCustomValidated)
+                {
+                    _io.WriteOutput(errorMessage ?? "Invalid characters");
+                    continue;
+                }
+            }
+            
+            isResolved = true;
+        }
+
+        return input;
+    }
 }
---
+++ MooGame.App/Interfaces/IUserInput.cs
@@ -0,0 +1,12 @@
+﻿namespace MooGame.App.Helper;
+
+public interface IUserInput
+{
+    string GetInput(
+        string? prompt = null,
+        string? errorMessage = null,
+        Func<string, bool>? customValidator = null
+    );
+
+    bool GetYesNo(string? question);
+}
