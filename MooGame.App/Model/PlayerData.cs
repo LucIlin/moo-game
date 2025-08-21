473f216d20dc58d9a3a64f0eab28a77018750f8d
Xanderelium <alexander_suleymanov@yahoo.se>
2025-08-21 14:43:45 +0200

    refactor: commented out something not important for this application right now

--- MooGame.App/Model/PlayerData.cs
+++ MooGame.App/Model/PlayerData.cs
@@ -1,41 +1,40 @@
-﻿namespace MooGame.App.Model
+﻿namespace MooGame.App.Model;
+
+class PlayerData //Ska metoderna i playerdata brytas ut eller tillhör de playerdata? kolla med chat
 {
-    class PlayerData //Ska metoderna i playerdata brytas ut eller tillhör de playerdata? kolla med chat
+    public string Name { get; private set; }
+    public int NumberOfGames { get; private set; } //NumberOfGames? Eller vad är det här?
+    int TotalNumberOfGuesses;
+
+
+    public PlayerData(string name, int guesses)
     {
-        public string Name { get; private set; }
-        public int NumberOfGames { get; private set; } //NumberOfGames? Eller vad är det här?
-        int TotalNumberOfGuesses;
+        Name = name;
+        NumberOfGames = 1;
+        TotalNumberOfGuesses = guesses;
+    }
+
+    public void Update(int guesses) //Bryt ut det här till en egen klass
+    {
+        TotalNumberOfGuesses += guesses;
+        NumberOfGames++;
+    }
+
+    public double Average() //PlayerDataController
+    {
+        return (double)TotalNumberOfGuesses / NumberOfGames;
+    }
 
 
-        public PlayerData(string name, int guesses)
-        {
-            Name = name;
-            NumberOfGames = 1;
-            TotalNumberOfGuesses = guesses;
-        }
-
-        public void Update(int guesses) //Bryt ut det här till en egen klass
-        {
-            TotalNumberOfGuesses += guesses;
-            NumberOfGames++;
-        }
-
-        public double Average() //PlayerDataController
-        {
-            return (double)TotalNumberOfGuesses / NumberOfGames;
-        }
+    //public override bool Equals(object p) //Har den någon användning?
+    //{
+    //    return Name.Equals(((PlayerData)p).Name);
+    //}
 
 
-        public override bool Equals(object p)
-        {
-            return Name.Equals(((PlayerData)p).Name);
-        }
-
-
-        public override int GetHashCode() //Ska man ta bort?
-        {
-            return Name.GetHashCode();
-        }
+    public override int GetHashCode() //Ska man ta bort?
+    {
+        return Name.GetHashCode();
     }
 }
 
