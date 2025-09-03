847a2c78c137873c59e930cc417b1575cc9bf7a5
Xanderelium <alexander_suleymanov@yahoo.se>
2025-09-03 10:53:09 +0200

    test: add MooUniqueNumberGeneratorTests for GenerateNumber and digit uniqueness
    - added tests verifying GenerateNumber returns string of correct length
    - added tests verifying generated digits are unique

---
+++ MooGame.Tests/Helper/MooUniqueNumberGeneratorTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MooGame.App.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MooGame.App.Helper.Tests
+{
+    [TestClass()]
+    public class MooUniqueNumberGeneratorTests
+    {
+        [DataTestMethod()]
+        [DataRow(1)]
+        [DataRow(4)]
+        [DataRow(8)]
+        public void GenerateNumber_Returns_String_Of_Requested_Length(int length)
+        {
+            //Arrange
+            var generator = new MooUniqueNumberGenerator();
+
+            //Act
+            var number = generator.GenerateNumber(length);
+
+            //Assert
+
+            Assert.IsNotNull(number, "Result should not be null.");
+            Assert.AreEqual(length, number.Length);
+        }
+
+        [DataTestMethod()]
+        [DataRow(1)]
+        [DataRow(4)]
+        [DataRow(8)]
+        public void Test_DigitsAreUnique(int length)
+        {
+            //Arrange
+            var generator = new MooUniqueNumberGenerator();
+            
+            //Act
+            var number = generator.GenerateNumber(length);
+            //Assert
+            Assert.AreEqual(length, number.Distinct().Count());
+        }
+
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+}
