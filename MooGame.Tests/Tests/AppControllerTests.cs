962d91159cdb881f39e6447fb54742c36967946a
Xanderelium <alexander_suleymanov@yahoo.se>
2025-09-03 11:30:00 +0200

    test: reset AppControllerTests after AppController refactor
    - removed outdated test logic that no longer matches AppController
    - added placeholder test class marked as "Not Done"
    - will add proper tests after dependent components are tested

---
+++ MooGame.Tests/Tests/AppControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using MooGame.App.Controller;
+using MooGame.Tests.Mocks;
+
+namespace MooGame.Tests.Tests;
+[TestClass]
+
+public sealed class AppControllerTests
+{
+    [TestMethod]
+    [TestCategory("Not Done")]
+    public void Test_CanRunProgram()
+    {
+        //Arrange
+        string[] inputs = { "luke", "1", "1234", "n" };
+        var userIO = MockHelpers.CreateUserInputHandler(inputs);
+        var gameFactory = new MockGameFactory(userIO);
+        var app = new AppController(gameFactory);
+
+        //Act
+        app.RunApplication();
+
+        //Assert
+        Assert.IsFalse(true);
+    }
+}
