c65c58cb01d0ca0d318b80627a22cd7e685ef067
Xanderelium <alexander_suleymanov@yahoo.se>
2025-09-03 11:26:42 +0200

    test: add GameController tests and supporting test doubles
    - add GameControllerTests (correct guess exits, outputs are correct, invalid inputs don’t count, two rounds when y/then n)
    - add MockHelpers (CreateUserInputHandler, CreateMooGame, CreateMockGame)
    - add MockGame implementing IGame with minimal behavior for tests
    - add MockGameFactory implementing IGameFactory returning a GameController with MockNumberGenerator
    - tag tests with [TestCategory("Done")]

---
+++ MooGame.Tests/Mocks/MockGameFactory.cs
@@ -0,0 +1,22 @@
+﻿using MooGame.App.Controller;
+using MooGame.App.Helper;
+using MooGame.App.Interfaces;
+using MooGame.App.Model;
+
+namespace MooGame.Tests.Mocks;
+
+public class MockGameFactory : IGameFactory
+{
+    public IUserInputHandler UserInput { get; set; }
+
+    public MockGameFactory(IUserInputHandler userInput)
+    {
+        UserInput = userInput;
+    }
+    public IGameController CreateGame()
+    {
+        var mockGen = new MockNumberGenerator("1234");
+        var mooGame = new MooGameEngine(mockGen);
+        return new GameController(mooGame, UserInput);
+    }
+}
---
+++ MooGame.Tests/Tests/GameControllerTests.cs
@@ -0,0 +1,107 @@
+﻿using MooGame.App.Controller;
+using MooGame.App.Helper;
+using MooGame.Tests.Mocks;
+
+namespace MooGame.Tests.Tests
+{
+    [TestClass()]
+    public class GameControllerTests
+    {
+
+        [TestMethod]
+        [TestCategory("Done")]
+        public void Test_PlayGame_CorrectGuess_ThenExitGame()
+        {
+            //Arrange
+            string[] userInputs = { "1234", "n" };
+            var userIO = MockHelpers.CreateUserInputHandler(userInputs);
+            var game = MockHelpers.CreateMockGame("1234");
+            var controller = new GameController(game, userIO);
+            //Act
+            controller.PlayGame();
+
+            //Assert
+            Assert.IsTrue(game.IsRoundOver);
+        }
+
+        [DataTestMethod]
+        [TestCategory("Done")]
+        [DataRow("INSTRUCTIONS")]
+        [DataRow("Continue? y/n")]
+        [DataRow("Correct, it took 1 guesses")]
+        public void Test_PlayGame_OutputsAreCorrect(string output)
+        {
+            //Arrange
+            var userInputs = new Queue<string>(new[] { "1234", "n" });
+            var io = new MockIO(userInputs);
+            var userInputHandler = new UserInputHandler(io);
+            var game = MockHelpers.CreateMockGame("1234");
+            var controller = new GameController(game, userInputHandler);
+
+            //Act
+            controller.PlayGame();
+
+            //Assert
+            Assert.IsTrue(io.Outputs.Any(o => o.Contains(output)));
+            Assert.IsTrue(io.Outputs.Any(o => o.Contains(output)));
+            Assert.IsTrue(io.Outputs.Any(o => o.Contains(output)));
+        }
+
+        [TestMethod]
+        [TestCategory("Done")]
+        public void Test_PlayGame_CorrectNumberOfGuesses()
+        {
+            //Arrange
+            string[] userInputs = { "1235", "1234", "n" };
+            var userIO = MockHelpers.CreateUserInputHandler(userInputs);
+            var game = MockHelpers.CreateMooGame("1234");
+            var controller = new GameController(game, userIO);
+            var expectedGuessCount = 2;
+
+            //Act
+            controller.PlayGame();
+            var actualGuessCount = game.GuessCount;
+
+            //Assert
+            Assert.AreEqual(expectedGuessCount, actualGuessCount);
+        }
+
+        [TestMethod]
+        [TestCategory("Done")]
+        public void PlayGame_InvalidInputs_DoNotCount()
+        {
+            //Arrange
+            var inputs = new[] { "x", "12", "abcd", "1234", "n" };
+            var userIO = MockHelpers.CreateUserInputHandler(inputs);
+            var game = MockHelpers.CreateMockGame("1234");
+            var controller = new GameController(game, userIO);
+            var expectedGuessCount = 1;
+
+            //Act
+            controller.PlayGame();
+            var actualGuessCount = game.GuessCount;
+
+            //Assert
+            Assert.AreEqual(expectedGuessCount, actualGuessCount);
+        }
+
+        [TestMethod]
+        [TestCategory("Done")]
+        public void PlayGame_TwoRounds_WhenYThenN()
+        {   
+            //Arrange
+            var inputs = new[] { "1234", "y", "1234", "n" };
+            var userIO = MockHelpers.CreateUserInputHandler(inputs);
+            MockGame game = MockHelpers.CreateMockGame("1234");
+            var controller = new GameController(game, userIO);
+            var expectedStartCount = 2;
+
+            //Act
+            controller.PlayGame();
+            var actualStartCounts = game.StartRoundCalls;
+
+            //Assert
+            Assert.AreEqual(expectedStartCount, actualStartCounts);
+        }
+    }
+}
